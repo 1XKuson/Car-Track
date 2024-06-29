50a62b13ac942bd3f4d0c328e813e209a90f6c03
ThEMaCcC <thanapat.mac06@gmail.com>
2024-06-29 18:36:53 +0700

    connect api

--- car-track-v1/src/App.jsx
+++ car-track-v1/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import ListCar from "./components/ListCar/ListCar";
 import { Outlet } from "react-router-dom";
 import Map from "./components/Map/Map";
@@ -8,12 +8,45 @@ import DetailCar from "./components/DetailCar/DetailCar";
 import TimeNow from "./components/TimeNow/TimeNow";
 import "./App.css"
 import "./Theme.css"
+import axios from "axios";
+
 const App = () => {
+
+  const [list, setList] = useState([])
+  const [position, setPosition] = useState([])
+
+  const fetchData = async () => {
+    try{
+        const response = await axios.get("http://161.246.160.72:3000/unicons")
+        setList(response.data.uniconIds)
+    } catch(error) {
+        console.log('error', error)
+    }
+  }
+
+  const fetchPosition = async () => {
+    try{
+        const response = await axios.get("http://161.246.160.72:3000/positions")
+        setPosition(response.data)
+    } catch(error) {
+        console.log('error', error)
+    }
+  }
+
+  useEffect(() => {
+      fetchData()
+      
+      const interval = setInterval(() => {
+        fetchPosition()
+      }, 1000);
   
+      return () => clearInterval(interval);
+  }, []) 
+
  return(
   <>
-    <Map />
-    <ListCar />
+    <Map position={position}/>
+    <ListCar list={list}/>
     {/* <DetailCar /> */}
     <TimeNow />
   </>
--- car-track-v1/src/components/DetailCar/DetailCar.jsx
+++ car-track-v1/src/components/DetailCar/DetailCar.jsx
@@ -1,132 +1,72 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import StationCard from "./StationCard";
 import "./DetailCar.css";
 import arrowForward from "../../Icon/arrowForward.png";
 import arrowBack from "../../Icon/arrowBack.png";
-
-/* const Station = [
-  {
-    time: "12:00",
-    station: "ตึก A",
-    detail: "สำนักงานคณบดีวิศวกรรมศาสตร์",
-  },
-  {
-    time: "12:10",
-    station: "ตึก B",
-    detail: "ภาควิชาวิศวกรรมอิเล็กทรอนิกส์",
-  },
-  {
-    time: "12:10",
-    station: "ตึก B",
-    detail: "ภาควิชาวิศวกรรมอิเล็กทรอนิกส์",
-  },
-  {
-    time: "12:10",
-    station: "ตึก B",
-    detail: "ภาควิชาวิศวกรรมอิเล็กทรอนิกส์",
-  },
-  {
-    time: "12:10",
-    station: "ตึก B",
-    detail: "ภาควิชาวิศวกรรมอิเล็กทรอนิกส์",
-  },
-  {
-    time: "12:10",
-    station: "ตึก B",
-    detail: "ภาควิชาวิศวกรรมอิเล็กทรอนิกส์",
-  },
-  {
-    time: "12:10",
-    station: "E12-อาคารเรียนรวม 12 ชั้น",
-    detail: "ภาควิชาวิศวกรรมระบบ",
-  },
-]; */
+import axios from "axios";
 
 const getTimeFromIsoDate = (isoDate) => {
   const date = new Date(isoDate);
   const hours = date.getHours().toString().padStart(2, '0');
   const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
   return `${hours-7}:${minutes}`;
 };
 
-const stations = [
-  {
-    date: "2024-05-31T05:30:00.000Z",
-    station: "E12-อาคารเรียนรวม 12 ชั้น",
-    lat: "13.8475",
-    lng: "100.5693",
-  },
-  {
-    date: "2024-05-31T06:00:00.000Z",
-    station: "ภาควิชาวิศวเครื่องกล",
-    lat: "13.7372",
-    lng: "100.5327",
-  },
-  {
-    date: "2024-05-31T07:00:00.000Z",
-    station: "ตึก A คณบดี",
-    lat: "13.7450",
-    lng: "100.5345",
-  },
-  {
-    date: "2024-05-31T08:00:00.000Z",
-    station: "ตึก ECC",
-    lat: "13.7413",
-    lng: "100.5328",
-  },
-  {
-    date: "2024-05-31T09:00:00.000Z",
-    station: "ตึกพระเทพ",
-    lat: "13.7436",
-    lng: "100.5332",
-  },
-];
-
-const Time = [
-  {
-    id: 1,
-    startStation: "ตึก A",
-    startTime: "12:00",
-    endStation: "ตึกโหล",
-    endTime: "13:00",
-  },
-  {
-    id: 2,
-    startStation: "ตึก A",
-    startTime: "13:00",
-    endStation: "ตึกโหล",
-    endTime: "14:00",
-  },
-  {
-    id: 3,
-    startStation: "ตึก A",
-    startTime: "14:00",
-    endStation: "ตึกโหล",
-    endTime: "15:00",
-  },
-  {
-    id: 4,
-    startStation: "ตึก A",
-    startTime: "15:00",
-    endStation: "ตึกโหล",
-    endTime: "16:00",
-  },
-  {
-    id: 5,
-    startStation: "ตึก A",
-    startTime: "16:00",
-    endStation: "ตึกโหล",
-    endTime: "17:00",
-  },
-];
+// const stations = [
+//   {
+//     date: "2024-05-31T05:30:00.000Z",
+//     station: "E12-อาคารเรียนรวม 12 ชั้น",
+//     lat: "13.8475",
+//     lng: "100.5693",
+//   },
+//   {
+//     date: "2024-05-31T06:00:00.000Z",
+//     station: "ภาควิชาวิศวเครื่องกล",
+//     lat: "13.7372",
+//     lng: "100.5327",
+//   },
+//   {
+//     date: "2024-05-31T07:00:00.000Z",
+//     station: "ตึก A คณบดี",
+//     lat: "13.7450",
+//     lng: "100.5345",
+//   },
+//   {
+//     date: "2024-05-31T08:00:00.000Z",
+//     station: "ตึก ECC",
+//     lat: "13.7413",
+//     lng: "100.5328",
+//   },
+//   {
+//     date: "2024-05-31T09:00:00.000Z",
+//     station: "ตึกพระเทพ",
+//     lat: "13.7436",
+//     lng: "100.5332",
+//   },
+// ];
 
 const DetailCar = ({ show, setShowSideNav, selectedCar }) => {
+
+  const [stations, setStations] = useState([])
+  
+  const fetchData = async () => {
+    try{
+        const response = await axios.get(`http://161.246.160.72:3000/route/${selectedCar}`)
+        setStations(response.data.stations)
+    } catch(error) {
+        console.log('error', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [selectedCar]) 
+  
   if (!selectedCar) {
     return null;
   }
-
-  const carDetails = Time.find((car) => car.id === selectedCar.id);
+  
+  
 
   return (
     <div className={`detailNav ${show ? "visible" : ""}`}>
@@ -143,7 +83,7 @@ const DetailCar = ({ show, setShowSideNav, selectedCar }) => {
 
       <div className="sideNav">
         <div className="insideNav">
-          <div className="headDetail">{selectedCar.name}</div>
+          <div className="headDetail">{selectedCar}</div>
           <table className="startEnd">
             <tbody>
               <tr>
