9fce99118b966f5457ff7c5f1a62cdf720f13941
KATANA0x00 <94747321+KATANA0x00@users.noreply.github.com>
2024-08-20 19:08:38 +0700

    research update

--- car-track-v1/src/App.jsx
+++ car-track-v1/src/App.jsx
@@ -1,16 +1,108 @@
+// import React, { useState, useEffect } from "react";
+// import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+// import ListCar from "./components/ListCar/ListCar";
+// import { Outlet } from "react-router-dom";
+// import Map from "./components/Map/BaseMap";
+// import DetailCar from "./components/DetailCar/DetailCar";
+// import TimeNow from "./components/TimeNow/TimeNow";
+// import Schedule from "./components/Schedule/Schedule";
+// import Logo from "./components/Logo/Logo";
+// import EditPage from "./components/EditPage/EditPage";
+// import "./App.css";
+// import "./Theme.css";
+// import axios from "axios";
+
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import gpage from './components/Map/Map';
+
+// const App = () => {
+//   const [list, setList] = useState([]);
+//   const [position, setPosition] = useState([]);
+//   const [center, setCenter] = useState();
+//   const [clientLocation, setClientLocation] = useState(null);
+
+//   const fetchData = async () => {
+//     try {
+//       const response = await axios.get("http://161.246.160.72:3000/unicons");
+//       setList(response.data.uniconIds);
+//     } catch (error) {
+//       // console.log("error", error);
+//     }
+//   };
+
+//   const fetchPosition = async () => {
+//     try {
+//       const response = await axios.get("http://161.246.160.72:3000/positions");
+//       setPosition(response.data);
+//       // console.log("success", response.data);
+//     } catch (error) {
+//       // console.log("error", error);
+//     }
+//   };
+
+//   function success(position) {
+//     const latitude = position.coords.latitude;
+//     const longitude = position.coords.longitude;
+//     setClientLocation({ lat: latitude, lng: longitude });
+//   }
+
+//   useEffect(() => {
+//     fetchData();
+//     setCenter({
+//       lat: 13.726639,
+//       lng: 100.774754
+//     })
+
+//     const interval = setInterval(() => {
+//       fetchPosition();
+//       if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(success);
+//       }
+//     }, 1000);
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   useEffect(() => {
+//     const script = document.createElement("script");
+//     script.async = true;
+//     script.src = "https://www.googletagmanager.com/gtag/js?id=G-5J4D7XWX6T";
+//     document.head.appendChild(script);
+
+//     script.onload = () => {
+//       window.dataLayer = window.dataLayer || [];
+//       function gtag() { window.dataLayer.push(arguments); }
+//       gtag('js', new Date());
+//       gtag('config', 'G-5J4D7XWX6T');
+//     };
+//   }, []);
+
+//   return (
+//     <>
+//       {/* <EditPage /> */}
+//       <Logo />
+//       <div className="test">* อยู่ในช่วงระหว่างการพัฒนาระบบ *</div>
+//       <Schedule />
+//       <Map positions={position} center={center} clientLocation={clientLocation} />
+//       <ListCar list={list} position={position} setCenter={setCenter} />
+//       {/* <DetailCar /> */}
+//       <TimeNow />
+//     </>
+//   );
+// };
+
+// export default App;
+
 import React, { useState, useEffect } from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ListCar from "./components/ListCar/ListCar";
-import { Outlet } from "react-router-dom";
 import Map from "./components/Map/BaseMap";
-import DetailCar from "./components/DetailCar/DetailCar";
-import TimeNow from "./components/TimeNow/TimeNow";
+import Gpage from './components/Map/Map';
 import Schedule from "./components/Schedule/Schedule";
 import Logo from "./components/Logo/Logo";
-import EditPage from "./components/EditPage/EditPage";
+import TimeNow from "./components/TimeNow/TimeNow";
+import axios from "axios";
 import "./App.css";
 import "./Theme.css";
-import axios from "axios";
 
 const App = () => {
   const [list, setList] = useState([]);
@@ -36,7 +128,6 @@ const App = () => {
       // console.log("error", error);
     }
   };
-
   function success(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
@@ -46,9 +137,9 @@ const App = () => {
   useEffect(() => {
     fetchData();
     setCenter({
-      lat: 13.726639, 
+      lat: 13.726639,
       lng: 100.774754
-    })
+    });
 
     const interval = setInterval(() => {
       fetchPosition();
@@ -74,16 +165,17 @@ const App = () => {
   }, []);
 
   return (
-    <>
-      {/* <EditPage /> */}
+    <Router>
       <Logo />
       <div className="test">* อยู่ในช่วงระหว่างการพัฒนาระบบ *</div>
       <Schedule />
-      <Map positions={position} center={center} clientLocation={clientLocation} />
+      <Routes>
+        <Route path="/" element={<Map positions={position} center={center} clientLocation={clientLocation} />} />
+        <Route path="/gmap" element={<Gpage position={position} center={center} setCenter={setCenter}/>} />
+      </Routes>
       <ListCar list={list} position={position} setCenter={setCenter} />
-      {/* <DetailCar /> */}
       <TimeNow />
-    </>
+    </Router>
   );
 };
 
